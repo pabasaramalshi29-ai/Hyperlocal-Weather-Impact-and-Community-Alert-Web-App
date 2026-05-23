4a6c7cd4ad286f4aaec8ea9bcc13d0f83d474a58
Heshan Atthanayake <heshanatthanayake@gmail.com>
2026-05-23 12:53:12 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBuBwo4fMVjzDGw6kA5faJl7dF5RcNbuVE",
+  authDomain: "hyperweather-696c8.firebaseapp.com",
+  projectId: "hyperweather-696c8",
+  storageBucket: "hyperweather-696c8.firebasestorage.app",
+  messagingSenderId: "307932801473",
+  appId: "1:307932801473:web:ca44b2400753ff40d12740",
+  measurementId: "G-BM0ZRVNWGF"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
